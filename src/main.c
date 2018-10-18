5060500328c8336fc78007692455e31144c19b39
vzwGrey <vzwgrey@gmail.com>
2018-10-18 19:56:36 +0200

    Add support for all options (except for -u) and refactor a bit

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
 
-int read_file(const char *file_name, char **buffer);
+void print_nonprint(unsigned char c);
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
@@ -9,42 +12,85 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	for (int i = 1; i < argc; i++) {
+	// Disable automatic error messages from getopt.
+	opterr = 0;
+
+	int b_flag = 0,
+		e_flag = 0,
+		n_flag = 0,
+		s_flag = 0,
+		t_flag = 0,
+		u_flag = 0,
+		v_flag = 0;
+
+	char opt;
+	while ((opt = getopt(argc, argv, "benstuv")) != -1) {
+		switch(opt) {
+			case 'b': b_flag = 1; break;
+			case 'e': v_flag = e_flag = 1; break;
+			case 'n': n_flag = 1; break;
+			case 's': s_flag = 1; break;
+			case 't': v_flag = t_flag = 1; break;
+			case 'u': u_flag = 1; break;
+			case 'v': v_flag = 1; break;
+		}	
+	}
+
+	for (int i = optind; i < argc; i++) {
 		char *file_name = argv[i];
-		char *content;
-		if (*file_name == '-') {
-			size_t len = 50;
-			while (getline(&content, &len, stdin) != -1)
-				printf("%s", content);
-			// Clear EOF flag (and other error flags) so stdin can be read again.
-			clearerr(stdin);
-		} else {
-			read_file(file_name, &content);
-			printf("%s", content);
+		FILE *file = strcmp(file_name, "-") == 0 ? stdin : fopen(file_name, "r");
+
+		int i = 1;
+		char *content = NULL;
+		size_t len = 0;
+		int last_blank;
+		while (getline(&content, &len, file) != -1) {
+			if (!s_flag || !last_blank || *content != '\n') {
+				if ((b_flag && *content != '\n') || n_flag)
+					printf("%6d ", i++);
+
+				if (!v_flag) {
+					printf("%s", content);
+				} else {
+					for (int j = 0; content[j]; j++) {
+						char c = content[j];
+						if ((c >= 32 && c <= 126) || c == '\n' || c == '\t') {
+							if (c == '\t' && t_flag)
+								printf("^I");
+							else if (c == '\n' && e_flag)
+								printf("$\n");
+							else
+								printf("%c", c);
+						} else {
+							print_nonprint(c);
+						}
+					}
+				}
+			}
+
+			if (*content == '\n')
+				last_blank = 1;
+			else
+				last_blank = 0;
 		}
+		
+		free(content);
+		fclose(file);
 	}
 
 	return 0;
 }
 
-int read_file(const char *file_name, char **buffer) {
-	FILE *file = fopen(file_name, "r");
-	if (!file) {
-		fprintf(stderr, "Could not open file %s.", file_name);
-		return 0;
-	}
-
-	fseek(file, 0, SEEK_END);
-	int size = ftell(file);
-	rewind(file);
-	*buffer = (char *) malloc(sizeof(char) * size);
-	if (!buffer) {
-		fprintf(stderr, "Could not allocate memory for file %s.", file_name);
-		return 0;
-	}
-	int read = fread(*buffer, 1, size, file);
-	fclose(file);
-
-	return read;
+void print_nonprint(unsigned char c) {
+	if (c < 32)
+		printf("^%c", c + 64);
+	else if (c == 127)
+		printf("^?");
+	else if (c < 160)
+		printf("M-^%c", c - 128 + 64);
+	else if (c < 255)
+		printf("M-%c", c - 160 + 32);
+	else
+		printf("M-^?");
 }
 
